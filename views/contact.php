46121450e80b767b20293c203f44775c43bc94bf
Agnes <agnes.guillet@hotmail.fr>
2021-11-22 16:42:26 +0100

    add theme bootstrap

--- views/contact.php
+++ views/contact.php
@@ -1,16 +1,20 @@
-<h3>Contact us</h3>
-<form action="" method="post">
-    <div class="mb-3">
-        <label for="subject" class="form-label">Subject</label>
-        <input type="text" class="form-control" name="subject" id="subject">
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input type="email" class="form-control" name="email" id="email">
-    </div>
-    <div class="mb-3">
-        <label for="message" class="form-label">Message</label>
-        <textarea class="form-control" name="message" id="message"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+<div class="container-fluid">
+    <h3 class="text-success text-center ">Contact us</h3>
+    <form action="" method="post" class="mx-auto">
+        <div class=" col-6 mb-3">
+            <label for="subject" class="col-form-label col-sm-2">Subject</label>
+            <input type="text" class="form-control bg-white" name="subject" id="subject">
+        </div>
+        <div class=" col-6 mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control bg-white" name="email" id="email">
+        </div>
+        <div class= "col-6 mb-3">
+            <label for="message" class="form-label">Message</label>
+            <textarea class="form-control bg-white" name="message" id="message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <a href="/"  class="btn btn-primary">Cancel</a>
+    </form>
+</div>
