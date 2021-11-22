e63f486f6bc7a1619b84997c6f6ca665f3586782
Agnes <agnes.guillet@hotmail.fr>
2021-11-22 21:56:05 +0100

    finish contact page bootstrap

--- views/contact.php
+++ views/contact.php
@@ -1,20 +1,23 @@
+<h3 class="text-success text-center ">Contact us</h3>
+<div class="container d-flex justify-content-center">
 
-<div class="container-fluid">
-    <h3 class="text-success text-center ">Contact us</h3>
-    <form action="" method="post" class="mx-auto">
-        <div class=" col-6 mb-3">
-            <label for="subject" class="col-form-label col-sm-2">Subject</label>
-            <input type="text" class="form-control bg-white" name="subject" id="subject">
-        </div>
-        <div class=" col-6 mb-3">
-            <label for="email" class="form-label">Email address</label>
-            <input type="email" class="form-control bg-white" name="email" id="email">
-        </div>
-        <div class= "col-6 mb-3">
-            <label for="message" class="form-label">Message</label>
-            <textarea class="form-control bg-white" name="message" id="message"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        <a href="/"  class="btn btn-primary">Cancel</a>
-    </form>
+
+        <form action="" method="post" class="w-50">
+            <div class="mb-3">
+                <label for="subject" class="form-label col-sm-2">Subject</label>
+                <input type="text" class="form-control bg-white" name="subject" id="subject">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control  bg-white" name="email" id="email">
+            </div>
+            <div class= " mb-3">
+                <label for="message" class="form-label">Message</label>
+                <textarea class="form-control bg-white" name="message" id="message"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <a href="/"  class="btn btn-primary">Cancel</a>
+        </form>
+
+
 </div>
