aa8f5a7e6d70ba430359104c25b6c537a31cea52
Agnes <agnes.guillet@hotmail.fr>
2021-11-23 10:46:49 +0100

    add boostrap them

--- views/contact.php
+++ views/contact.php
@@ -1,16 +1,23 @@
-<h3>Contact us</h3>
-<form action="" method="post">
-    <div class="mb-3">
-        <label for="subject" class="form-label">Subject</label>
-        <input type="text" class="form-control" name="subject" id="subject">
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input type="email" class="form-control" name="email" id="email">
-    </div>
-    <div class="mb-3">
-        <label for="message" class="form-label">Message</label>
-        <textarea class="form-control" name="message" id="message"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<h3 class="text-success text-center ">Contact us</h3>
+<div class="container d-flex justify-content-center">
+
+
+    <form action="" method="post" class="w-50">
+        <div class="mb-3">
+            <label for="subject" class="form-label col-sm-2">Subject</label>
+            <input type="text" class="form-control bg-white" name="subject" id="subject">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control  bg-white" name="email" id="email">
+        </div>
+        <div class= " mb-3">
+            <label for="message" class="form-label">Message</label>
+            <textarea class="form-control bg-white" name="message" id="message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <a href="/"  class="btn btn-primary">Cancel</a>
+    </form>
+
+
+</div>
